d8284f5f3bab5ec000af672bbb2b210281955bab
Owen <owen.allemang@gmail.com>
2021-01-03 18:24:30 +0000

    clean up test code b4 func

--- main.py
+++ main.py
@@ -76,48 +76,6 @@ def network_graph(country, airports_names=airports_names, routes=routes_tot):
     plt.show()
 
 
-airports_us = airports_names[airports_names["country"] == "United States"]
-airports_us = airports_us[airports_us["Lon"] != -70]
-routes_us = routes_2003[routes_2003["Source Country"] == "USA"]
-routes_us = routes_us.append(routes_2016[routes_2016["Source Country"] == "USA"], ignore_index=True)
-
-#       Geographic visualisation
-
-# Remove missing airports from the routes
-routes_us = routes_us[routes_us["Source"].isin(airports_us.index)]
-routes_us = routes_us[routes_us["Target"].isin(airports_us.index)]
-
-edges = routes_us[["Source", "Target"]].values
-g = nx.from_edgelist(edges)
-
-pos = {airport: (v["Lon"], v["Lat "]) for airport, v in
-       airports_us.to_dict('index').items()}
-
-deg = nx.degree(g)
-sizes = [2 * deg[iata] for iata in g.nodes]
-
-labels = {iata: iata if deg[iata] >= 32 else ''
-          for iata in g.nodes}
-
-crs = ccrs.PlateCarree()
-fig, ax = plt.subplots(
-    1, 1, figsize=(12, 8),
-    subplot_kw=dict(projection=crs))
-ax.coastlines()
-# Extent of continental US.
-ax.set_extent([-179, -66, 15, 59])
-ax.gridlines()
-nx.draw_networkx(g, ax=ax,
-                 font_size=15,
-                 alpha=.5,
-                 width=.075,
-                 node_size=sizes,
-                 labels=labels,
-                 pos=pos,
-                 node_color=sizes,
-                 cmap=plt.cm.plasma)
-plt.show()
-
 ## plot f degree distribution
 
 ## degree vs betweenness distr
