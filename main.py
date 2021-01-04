b2c01d7d3915a6302b96527bcceedf12cce7f145
Owen <owen.allemang@gmail.com>
2021-01-04 00:55:34 +0000

    edges weight added and new/old for the flights

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import collections
 from IPython.display import Image
 
 COUNTRIES = ["USA", "China", "United Kingdom", "Australia"]
-COORDINATES = {"USA": [-179, -66, 10, 55],
+COORDINATES = {"USA": [-179, -66, 16, 60],
                "China": [70, 140, 15, 53],
                "United Kingdom": [-11, 2, 48, 60],
                "Australia": [111, 155, -42, -9]}
@@ -27,6 +28,9 @@ routes_2016 = routes_2016[routes_2016["Source Country"].isin(COUNTRIES)].reset_i
 
 # ignoring time series as 2016 doesn't have it
 routes_2003 = routes_2003.drop(["TimeSeries"], axis=1)
+# instead add a columns to differentiate between old (2003-2009) and new (2010-2016)
+routes_2003["isOld"] = 1
+routes_2016["isOld"] = 0
 
 # join the two routes in one df
 routes_tot = routes_2003.append(routes_2016, ignore_index=True)
@@ -43,8 +47,9 @@ def network_graph(country, airports_names=airports_names, routes=routes_tot):
     routes_country = routes_country[routes_country["Source"].isin(airports_country.index) &
                                     routes_country["Target"].isin(airports_country.index)]
 
-    edges = routes_country[["Source", "Target"]].values
-    g = nx.from_edgelist(edges)
+    weight_edges = routes_country[["Source", "Target", "Weight"]].values
+    g = nx.Graph()
+    g.add_weighted_edges_from(weight_edges)
 
     pos = {airport: (v["Lon"], v["Lat "]) for airport, v in
            airports_country.to_dict('index').items()}
@@ -54,6 +59,10 @@ def network_graph(country, airports_names=airports_names, routes=routes_tot):
 
     labels = {iata: iata if deg[iata] >= 32 else ''
               for iata in g.nodes}
+
+    all_weights = [int(data['weight']) for node1, node2, data in g.edges(data=True)]
+    edge_width = [(((weight - min(all_weights)) * (1 - 0.075)) / (max(all_weights) - min(all_weights))) + 0.075
+                  for weight in all_weights]
 
     crs = ccrs.PlateCarree()
     fig, ax = plt.subplots(
@@ -67,7 +76,7 @@ def network_graph(country, airports_names=airports_names, routes=routes_tot):
     nx.draw_networkx(g, ax=ax,
                      font_size=15,
                      alpha=.5,
-                     width=.075,
+                     width=edge_width,
                      node_size=sizes,
                      labels=labels,
                      pos=pos,
@@ -76,10 +85,27 @@ def network_graph(country, airports_names=airports_names, routes=routes_tot):
     plt.show()
 
 
-## plot f degree distribution
+# plot f degree distribution
+def degree_distribution(G):
+    degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
+    degree_count = collections.Counter(degree_sequence)
+    deg, cnt = zip(*degree_count.items())
 
-## degree vs betweenness distr
 
-## assortativity
+# degree vs betweenness distr
+def degree_betweenness(G):
+    pass
 
-## core community size
+
+# assortativity
+def assort(G):
+    pass
+
+
+# core community size
+def core_community(G):
+    pass
+
+
+if __name__ == "__main__":
+    pass
